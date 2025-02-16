21aaaac529d45888be588a6c53e097e00a46a11b
Yusufbek-uz <yusufbekdeveloper@gmail.com>
2025-02-16 11:54:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let typed = new Typed('#element', {
+    strings: ['Lorem ipsum dolor sit amet, consectetur adipiscing elit','Purus interdum purus, est tortor pulvinarut in.'],
+    loopCount: Infinity,
+    typeSpeed: 50,
+    cursorChar: '',
+    loop: true,
+});
